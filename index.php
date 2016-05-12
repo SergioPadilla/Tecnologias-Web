fa88cb4ce2b286c723466e5d5cd317225314b014
SergioPadilla <splsergiopadilla@gmail.com>
2016-05-13 01:11:25 +0200

    cambiado el tamaño de letra

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             <p id="fecha">
                 <script>getDate()</script> <noscript>Error Javascript</noscript>
             </p>
-            <iframe id="hora" src="http://free.timeanddate.com/clock/i5700uuq/n2322/tles4/fn15/fs20/fcfff/tct/pct/ftb/th1" frameborder="0" width="76" height="25" allowTransparency="true"></iframe>
+        <iframe id="hora" src="http://free.timeanddate.com/clock/i5700uuq/n2322/tles4/fn15/fs39/fcfff/tct/pct/th1" frameborder="0" width="141" height="47" allowTransparency="true"></iframe>
     </div>
 
     <div class="cuerpo">
