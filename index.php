7c2006f338165ca22bab8c1b5175e19158b37f1e
SergioPadilla <splsergiopadilla@gmail.com>
2016-05-13 01:02:25 +0200

    actualizada cabecera

--- index.php
+++ index.php
@@ -7,21 +7,15 @@
 
 <Body>
     <div class="cabecera">
-        <div class="cabecera_division">
             <img id="logo" src="imagenes/logo.png" ALT="rowhard" align="middle">
-        </div>
-        <div class="cabecera_division">
             <p id="fecha">
                 <script>getDate()</script> <noscript>Error Javascript</noscript>
             </p>
-        </div>
-        <div class="cabecera_division">
             <iframe id="hora" src="http://free.timeanddate.com/clock/i5700uuq/n2322/tles4/fn15/fs20/fcfff/tct/pct/ftb/th1" frameborder="0" width="76" height="25" allowTransparency="true"></iframe>
-        </div>
     </div>
 
     <div class="cuerpo">
-
+        
     </div>
 
     <div class="pie">
