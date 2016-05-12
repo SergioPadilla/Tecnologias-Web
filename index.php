2e055c9bd0ec69796daafee216bff4742326e6d9
SergioPadilla <splsergiopadilla@gmail.com>
2016-05-12 19:54:14 +0200

    main structure

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<Head>
+
+</Head>
+
+<Body>
+    <div class="head">
+        <h1>Título</h1>
+    </div>
+
+    <div class="body">
+
+    </div>
+
+    <div class="pie">
+
+    </div>
+    <?php
+    ?>
+</Body>
