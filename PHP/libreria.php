8e4d3b9a53971083fe5af3a20250aba661ffe757
SergioPadilla <splsergiopadilla@gmail.com>
2016-05-13 23:32:28 +0200

    reestructuracion libreria.php

---
+++ PHP/libreria.php
@@ -0,0 +1,52 @@
+<?php
+/********************************************************************************************/
+/*			Clase para controlar la base de datos MySQL*/
+/********************************************************************************************/
+
+class Servidor_Base_Datos{
+    private $servidor, $usuario, $pass, $base_datos, $descriptor;
+    private $resultado;
+
+    function __construct($ser,$usu,$passw,$base){
+        $this->servidor=$ser;
+        $this->usuario=$usu;
+        $this->pass=$passw;
+        $this->base_datos=$base;
+        $this->conectar_base_datos();
+    }
+
+    private function conectar_base_datos(){
+        $this->descriptor=mysqli_connect($this->servidor,$this->usuario,$this->pass,$this->base_datos);
+    }
+
+    public function consulta($consulta){
+        $this->resultado=mysqli_query($this->descriptor,$consulta);
+    }
+
+    public function extraer_registro(){
+        if($fila=mysqli_fetch_array($this->resultado,MYSQLI_ASSOC)){
+            return $fila;
+        } else {
+            return false;
+        }
+    }
+
+    public function extraer_registro_indice($indice){
+        mysqli_data_seek($this->resultado,$indice);
+        $fila=mysqli_fetch_array($this->resultado,MYSQLI_ASSOC);
+        return ($fila);
+    }
+
+    public function numero_filas(){
+        return mysqli_num_rows($this->resultado);
+    }
+
+    function __destruct(){
+        mysqli_close($this->descriptor);
+    }
+}
+
+/********************************************************************************************/
+/*			Fin Clase para controlar la base de datos SQL*/
+/********************************************************************************************/
+?>
