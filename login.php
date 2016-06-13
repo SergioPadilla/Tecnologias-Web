4393154776097a21bef70258f57329558f51ee7c
SergioPadilla <splsergiopadilla@gmail.com>
2016-06-13 23:12:53 +0200

    borrado boton recuerdame

--- login.php
+++ login.php
@@ -6,7 +6,6 @@
 require_once('Configuracion/config.php');
 require_once('PHP/libreria.php');
 
-$_COOKIE['remember_me']=""; // mirar cookies
 $_SESSION[AUTENTICADO] = 'no';
 
 $mensaje="";
@@ -22,20 +21,6 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $conexion->consulta($sql);
 
     if($conexion->numero_filas()!=0){
-        /**
-         * Control de sesión si pincha en "recuerdame"
-         */
-        if($_POST['remember']) {
-            $year = time() + 31536000;
-            setcookie('remember_me', $_POST['user'], $year, "/");
-        }
-        elseif(!$_POST['remember']) {
-            if(isset($_COOKIE['remember_me'])) {
-                $past = time() - 100;
-                setcookie(remember_me, gone, $past);
-            }
-        }
-
         // Guarda sesión y accede a la siguiente página
         $_SESSION[USUARIO]=$_POST['user'];
         $_SESSION[AUTENTICADO]="si";
@@ -77,24 +62,13 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 <body>
     <!-- Contenedor principal -->
     <div class="container">
-
         <form class="form-signin" method="post" action="login.php" >
             <h2 class="form-signin-heading">Inicio Sesión</h2>
             <input name = "user" type="text" id="inputEmail" class="form-control" placeholder="Usuario"
                    required autofocus maxlength="20" value="<?php echo $_COOKIE['remember_me']; ?>">
             <input name = "password" type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required maxlength="20">
             <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="remember"
-                           <?php
-                           if(isset($_COOKIE['remember_me']))
-                                echo 'checked="checked"';
-                           else
-                                echo '';
-                           ?>
-                    > Recuérdame
-                </label>
-                <a id="registro" href="register.php"> Crear cuenta </a> <!-- Opcion para registrarse -->
+                <a id="registro" href="register.php" style="margin-bottom: 10px"> Crear cuenta </a> <!-- Opcion para registrarse -->
             </div>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Aceptar</button>
 
